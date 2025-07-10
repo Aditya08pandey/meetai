62dd32707b4842fe9f16c889921dc9f8e0789965
Aditya Pandey <pandeydevil0802@gmail.com>
2025-07-10 13:03:02 +0530

    bug fix

--- src/modules/meetings/ui/components/transcript.tsx
+++ src/modules/meetings/ui/components/transcript.tsx
@@ -68,7 +68,7 @@ export const Transcript = ({meetingId}: Props) => {
                             </div>
                         )
                     })}
-                </div>C
+                </div>
             </ScrollArea>
 
         </div>
